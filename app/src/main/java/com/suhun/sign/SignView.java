683ff6c8da01c827b352d234b117f95fc7e6850d
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-04-20 08:46:50 +0800

    Implement draw many lines to show user touch track

--- app/src/main/java/com/suhun/sign/SignView.java
+++ app/src/main/java/com/suhun/sign/SignView.java
@@ -16,7 +16,7 @@ import java.util.LinkedList;
 
 public class SignView extends View {
     public String tag = SignView.class.getSimpleName();
-    private LinkedList<HashMap<String, Float>> line = new LinkedList();
+    private LinkedList<LinkedList<HashMap<String, Float>>> lines = new LinkedList<>();
     public SignView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
@@ -24,27 +24,35 @@ public class SignView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Log.d(tag, "---onDraw---");
         setBackgroundColor(Color.GREEN);
+        int painStrokeWidth = 5;
         Paint paint = new Paint();
         paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(4);
-        for(int i=1;i<line.size();i++){
-            HashMap<String, Float> p0 = line.get(i-1);
-            HashMap<String, Float> p1 = line.get(i);
-            canvas.drawLine(p0.get("x"), p0.get("y"), p1.get("x"), p1.get("y"), paint);
+        paint.setStrokeWidth(painStrokeWidth);
+        for(LinkedList<HashMap<String, Float>>line:lines){
+            for(int i=1;i<line.size();i++){
+                HashMap<String, Float> po = line.get(i-1);
+                HashMap<String, Float> p1 = line.get(i);
+                canvas.drawLine(po.get("x"), po.get("y"), p1.get("x"), p1.get("y"), paint);
+            }
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float x = event.getX(), y = event.getY();
-        Log.d(tag, "---touch---"+"x:"+ x+"Y"+y);
+        LinkedList<HashMap<String, Float>> line;
         HashMap<String, Float> point = new HashMap<>();
-        point.put("x",x);point.put("y",y);
-        line.add(point);
-        invalidate();
+        float x = event.getX(), y = event.getY();
+        point.put("x", x); point.put("y", y); //record user touch coordinate x and y and save to HashMap
+        if(event.getAction() == MotionEvent.ACTION_DOWN){//Draw new line
+            Log.d(tag, "Draw new line in position x:" + x + "y:" + y);
+            line = new LinkedList<>();
+            line.add(point);
+            lines.add(line);
+        }else if(event.getAction() == MotionEvent.ACTION_MOVE){//user touch and move in last line
+            lines.getLast().add(point);
+        }
+        invalidate(); //reload view for onDraw
         return true;
-//        return super.onTouchEvent(event);
     }
 }
